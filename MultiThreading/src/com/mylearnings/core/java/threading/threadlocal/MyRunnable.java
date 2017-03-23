0a2ec8b5a1c057f550777a551b740a4c80a38bc6
Prem <premkmishra.info@gmail.com>
2017-03-23 17:29:03 +0530

    Modified

--- MultiThreading/src/com/mylearnings/core/java/threading/threadlocal/MyRunnable.java
+++ MultiThreading/src/com/mylearnings/core/java/threading/threadlocal/MyRunnable.java
@@ -4,25 +4,33 @@ import java.util.Random;
 
 public class MyRunnable implements Runnable {
 
-	private ThreadLocal<String> threadLocal = new ThreadLocal<>();
+//	private ThreadLocal<String> threadLocal = new ThreadLocal<>();
+	
+	private ThreadLocal<String> threadLocalInitialVal = new ThreadLocal<String>(){
+		@Override
+		protected String initialValue() {
+	        return "This is the initial value";
+	    }
+	};
 
 	@Override
 	public void run() {
+		System.out.println("Thread " + Thread.currentThread().getName() + " initial value:- " +threadLocalInitialVal.get());
 		// There will be only one element per thread in LocalThread. No matters how many elements has been added.
 		// it simply use to set the value in ThreadLocalMap where key is thread itself.
-		threadLocal.set(Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
-		threadLocal.set(Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
-		threadLocal.set(Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
-		threadLocal.set(Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
+		threadLocalInitialVal.set("Thread " +Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
+		threadLocalInitialVal.set("Thread " +Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
+		threadLocalInitialVal.set("Thread " +Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
+		threadLocalInitialVal.set("Thread " +Thread.currentThread().getName() + " generates " + new Random().nextInt(100));
 		
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println(threadLocal.get());
-		System.out.println(threadLocal.get());
-		System.out.println(threadLocal.get());
+		System.out.println(threadLocalInitialVal.get());
+		System.out.println(threadLocalInitialVal.get());
+		System.out.println(threadLocalInitialVal.get());
 	}
 
 }
